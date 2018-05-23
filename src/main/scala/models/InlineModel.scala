be9b9d4bbad907b086b28ecb90d098fea3861c37
AshidaSRS <ashida.shin@gmail.com>
2018-05-23 10:27:08 +0200

    Refactor

--- src/main/scala/models/InlineModel.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-/**
-  * Created by ashida on 20/05/17.
-  */
-case class URL(a: String)
-case class CallBack(a: String)
-case class Query(a: String)
-case class QueryChat(a: String)
